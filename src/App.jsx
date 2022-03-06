90af05aacaff22dbf846045d36bd16cf816669c7
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-03-06 09:57:36 +0530

    created Routes for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ForgotPassword } from './ForgotPassword';
+import { Explore } from './pages/Explore';
+import { Offers } from './pages/Offers';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
 
 function App() {
   return (
-   <h1>My app</h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore></Explore>}></Route>
+          <Route path="/offers" element={<Offers></Offers>}></Route>
+          <Route path="/profile" element={<SignIn></SignIn>}></Route>
+          <Route path="/sign-In" element={<SignIn></SignIn>}></Route>
+          <Route path="/sign-Up" element={<SignUp></SignUp>}></Route>
+          <Route
+            path="/forgotPassword"
+            element={<ForgotPassword></ForgotPassword>}
+          ></Route>
+        </Routes>
+        {/* space for NavBar */}
+      </Router>
+    </>
   );
 }
 
