efe5eeb085adcff475b34d5cca84b7beb56dcab5
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-03-07 01:50:17 +0530

    done for the day!!!

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -13,7 +13,33 @@ import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
 import { Spinner } from '../components/Spinner';
 
-
 export const Category = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(()=>{
+
+    const fetchListing = async () =>{
+        try {
+
+            //get a reference
+
+            const listingsRef = collection(db, "listings")
+
+            //create a query
+
+            
+
+        } catch (error) {
+            
+        }
+    }
+
+    fetchListing()
+
+  },[])
+
   return <div>Category</div>;
 };
