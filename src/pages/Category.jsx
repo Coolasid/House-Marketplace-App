100915219e2c62ac8575958b9a0e541d63f6c0bd
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-03-07 21:06:02 +0530

    listing page created

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import {
   collection,
-  getDoc,
+  getDocs,
   query,
   where,
   orderBy,
@@ -12,34 +12,82 @@ import {
 import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
 import { Spinner } from '../components/Spinner';
+import { ListingItem } from '../components/ListingItem';
+
 
 export const Category = () => {
-  const [listing, setListing] = useState(null);
+  const [listings, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
 
   const params = useParams();
 
-  useEffect(()=>{
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        //get a reference
 
-    const fetchListing = async () =>{
-        try {
+        const listingsRef = collection(db, 'listings');
 
-            //get a reference
+        //create a query
 
-            const listingsRef = collection(db, "listings")
+        const q = query(
+          listingsRef,
+          where('type', '==', params.categoryName),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        );
 
-            //create a query
+        //execute query
 
-            
+        const querySnap = await getDocs(q);
 
-        } catch (error) {
-            
-        }
-    }
+        const listings = [];
 
-    fetchListing()
+        querySnap.forEach((doc) => {
+          // console.log(doc.data());
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
 
-  },[])
+        setListing(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Colud not fetch listing');
+      }
+    };
 
-  return <div>Category</div>;
+    fetchListing();
+  }, []);
+
+  return (
+    <div className="category">
+      <header>
+        <div className="pageHeader">
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}
+        </div>
+      </header>
+
+      {loading ? (
+        <Spinner></Spinner>
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+              <ul className="categoryListings">
+                  {listings.map((listing)=>{
+                   return <ListingItem listing={listing.data} id={listing.id} key={listing.id} ></ListingItem>
+                  })}
+                </ul>
+
+          </main>
+        
+        </>
+      ) : (
+        <p> No listing for {params.categoryName} </p>
+      )}
+    </div>
+  );
 };
